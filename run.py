b39ee6df7cc1234fb0e1f8d9be23aaf942d7c0dc
Louis Marganne <margannelouis@gmail.com>
2022-06-09 16:15:34 +0200

    Added generation of TPS file

--- run.py
+++ run.py
@@ -19,7 +19,7 @@
 __author__ = "Marganne Louis <louis.marganne@student.uliege.be>"
 
 
-from cytomine.models import ImageInstanceCollection, Job, AttachedFileCollection, Annotation, AnnotationCollection
+from cytomine.models import ImageInstanceCollection, Job, AttachedFileCollection, Annotation, AnnotationCollection, JobData, TermCollection
 from cytomine import CytomineJob
 
 from tensorflow.keras.models import load_model
@@ -77,19 +77,24 @@ def main(argv):
 		pred_images.sort()
 		
 
-		# Fetch data from the trained model
-		tr_model_job = Job().fetch(cj.parameters.model_to_use)
-		attached_files = AttachedFileCollection(tr_model_job).fetch()
-		tr_model = find_by_attribute(attached_files, 'filename', '%d_model.hdf5' % cj.parameters.model_to_use)
-		tr_model_filepath = in_path + '%d_model.hdf5' % cj.parameters.model_to_use
-		tr_model.download(tr_model_filepath, override=True)
-		tr_parameters = find_by_attribute(attached_files, 'filename', '%d_parameters.joblib' % cj.parameters.model_to_use)
-		tr_parameters_filepath = in_path + '%d_parameters.joblib' % cj.parameters.model_to_use
-		tr_parameters.download(tr_parameters_filepath, override=True)
+		# Fetch data from the trained model(s)
+		models = []
+		parameters_hash = []
 
-		# Load fetched data
-		model = load_model(tr_model_filepath)
-		parameters_hash = joblib.load(tr_parameters_filepath)
+		jobs_ids = [int(job_id) for job_id in cj.parameters.model_to_use.split(',')]
+		for job_id in jobs_ids:
+			tr_model_job = Job().fetch(cj.parameters.model_to_use)
+			attached_files = AttachedFileCollection(tr_model_job).fetch()
+			tr_model = find_by_attribute(attached_files, 'filename', '%d_model.hdf5' % cj.parameters.model_to_use)
+			tr_model_filepath = in_path + '%d_model.hdf5' % cj.parameters.model_to_use
+			tr_model.download(tr_model_filepath, override=True)
+			tr_parameters = find_by_attribute(attached_files, 'filename', '%d_parameters.joblib' % cj.parameters.model_to_use)
+			tr_parameters_filepath = in_path + '%d_parameters.joblib' % cj.parameters.model_to_use
+			tr_parameters.download(tr_parameters_filepath, override=True)
+
+			# Load fetched data
+			models.append(load_model(tr_model_filepath))
+			parameters_hash.append(joblib.load(tr_parameters_filepath))
 
 
 		## 3. Download the images
@@ -110,46 +115,85 @@ def main(argv):
 			cv.imwrite(rescaled_images_path+im_name+'.png', re_img)
 
 
-		## 5. Construct testing set with tensorflow
-		test_images = glob.glob(rescaled_images_path+'*.png')
-
-		test_ds = tf.data.Dataset.from_tensor_slices((test_images, None))
-		test_ds = test_ds.map(lambda image, lm: parse_data(image, lm), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-		test_ds = test_ds.map(lambda image, lm: normalize(image, lm), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-		test_ds = test_ds.batch(parameters_hash['model_batch_size'])
-
-		
-		## 6. Predict landmarks
+		## 5. Construct testing set with tensorflow and predict landmarks for each model
 		cj.job.update(progress=80, statusComment='Predicting landmarks...')
 
-		# Predict using the model
-		preds = model.predict(test_ds)
+		test_images = glob.glob(rescaled_images_path+'*.png')
 
-		# Upscale prediction to original size
-		pred_landmarks = []
-		for i in range(len(test_images)):
-			org_img = cv.imread(org_images[i])
-			pred_mask = preds[i]
-			pred_mask = np.reshape(pred_mask, newshape=(256,256, parameters_hash['N']))
+		pred_landmarks_list = []
+		for i in range(len(jobs_ids)):
+			test_ds = tf.data.Dataset.from_tensor_slices((test_images, None))
+			test_ds = test_ds.map(lambda image, lm: parse_data(image, lm), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+			test_ds = test_ds.map(lambda image, lm: normalize(image, lm), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+			test_ds = test_ds.batch(parameters_hash[i]['model_batch_size'])
 
-			lm_list = []
-			for j in range(parameters_hash['N']):
-				x, y =  maskToKeypoints(pred_mask[:, :, j])
-				lm_list.append((x, y))
-			
-			pred_lm = np.array(lm_list)
-			up_size = org_img.shape[:2]
-			up_lmks = up_lm(pred_lm, 256, up_size)
-			pred_landmarks.append(up_lmks)
+			# Predict using the model
+			preds = models[i].predict(test_ds)
+
+			# Upscale prediction to original size
+			pred_landmarks = []
+			for j in range(len(test_images)):
+				org_img = cv.imread(org_images[j])
+				pred_mask = preds[j]
+				pred_mask = np.reshape(pred_mask, newshape=(256,256, parameters_hash[i]['N']))
+
+				lm_list = []
+				for k in range(parameters_hash[i]['N']):
+					x, y =  maskToKeypoints(pred_mask[:, :, k])
+					lm_list.append((x, y))
+				
+				pred_lm = np.array(lm_list)
+				up_size = org_img.shape[:2]
+				up_lmks = up_lm(pred_lm, 256, up_size)
+				pred_landmarks.append(up_lmks)
+
+			pred_landmarks_list.append(pred_landmarks)
 
 
-		## 7. Save landmarks as annotations in Cytomine
+		## 6. Save landmarks as annotations in Cytomine
 		annotation_collection = AnnotationCollection()
-		for i, image in enumerate(pred_images):
-			for j in range(parameters_hash['N']):
-				lm = Point(pred_landmarks[i][j][0], image.height - pred_landmarks[i][j][1])
-				annotation_collection.append(Annotation(location=lm.wkt, id_image=image.id, id_terms=[parameters_hash['cytomine_id_terms'][j]], id_project=cj.parameters.cytomine_id_project))
+		for i in range(len(jobs_ids)):
+			for j, image in enumerate(pred_images):
+				for k in range(parameters_hash[i]['N']):
+					lm = Point(pred_landmarks[i][j][k][0], image.height - pred_landmarks[i][j][k][1])
+					annotation_collection.append(Annotation(location=lm.wkt, id_image=image.id, id_terms=[parameters_hash[i]['cytomine_id_terms'][k]], id_project=cj.parameters.cytomine_id_project))
 		annotation_collection.save()
+
+
+		## 7. Save prediction in a TPS file
+		# Construct dictionnaries mapping image/term name to image/name id
+		terms = TermCollection().fetch_with_filter("project", cj.parameters.cytomine_id_project)
+		terms_names = {term.id : term.name for term in terms}
+
+		with open(f'{cj.job.id}.TPS', 'w') as f:
+			# Compute total number of landmarks
+			LM = 0
+			for i in range(len(jobs_ids)):
+				LM += parameters_hash[i]['N']
+
+			for i, landmarks in enumerate(zip(*pred_landmarks_list)):
+				# Get info about current image
+				image_name = pred_images[i].filename
+				image_height = pred_images[i].height
+
+				# Sort landmarks in accordance to their name
+				lm = np.concatenate(*landmarks)
+				lm_ids = np.concatenate(parameters_hash[i]['cytomine_id_terms'])
+				lm_names = [terms_names[lm_id] for lm_id in lm_ids]
+				lm = lm[np.argsort(lm_names)]
+
+				# Write number of landmarks
+				f.write(f'LM={LM}\n')
+
+				# Write landmarks
+				for landmark in lm:
+					file.write("%.5f %.5f\n" % (landmark[0], image_height - landmark[1]))
+
+				# Write image name and scale
+				file.write('IMAGE='+image_name+'\n')
+				file.write('SCALE=x.xxxxxx\n')
+
+
 
 		cj.job.update(status=Job.TERMINATED, progress=100, statusComment='Job terminated.')
 
